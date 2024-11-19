5a0b7c2d17583f3bad7ec196eb3cb18fc9017c1f
SufficientFlow <sufficientflow@protonmail.com>
2024-11-20 00:55:37 +0100

    chore: restructuring of website

--- src/app/dashboard/[serverid]/panel/page.tsx
+++
@@ -1,7 +0,0 @@
-export default async function Panel({ params }: { params: { serverid: number } }) {
-    const { serverid } = await params;
-    
-    return (
-        <h1>Panel with ID: {serverid}</h1>
-    );
-}
